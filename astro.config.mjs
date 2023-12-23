c8ee3a8e5d764ec6a0cab633ba962a83abc5eadb
kvdpxne <kvdpxne@gmail.com>
2023-12-23 22:57:08 +0100

    Minor changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    assets: "_assets"
-  },
-  integrations: [
-    tailwind()
-  ]
+	build: {
+		assets: "_assets"
+	},
+	integrations: [
+		tailwind()
+	],
+	i18n: {
+		defaultLocale: "en",
+		locales: [
+			"en", "pl"
+		],
+		routing: {
+			prefixDefaultLocale: true
+		}
+	}
 });
