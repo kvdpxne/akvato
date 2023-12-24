af846414333dc06b54ccc3544cf97aeb9f998619
kvdpxne <kvdpxne@gmail.com>
2023-12-25 00:12:09 +0100

    Updated translations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import {defineConfig} from "astro/config";
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://akvato.com",
 	build: {
 		assets: "_assets"
 	},
 	integrations: [
+		sitemap({
+			i18n: {
+				defaultLocale: "en",
+				locales: {
+					en: "en", // The `defaultLocale` value must present in `locales` keys
+					pl: "pl"
+				}
+			}
+		}),
 		astroI18next(),
 		tailwind()
 	]
